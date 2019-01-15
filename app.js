9343ef682c9595b25777baf7d547acfd107e0f81
adanlessossi <adanlessossi@hotmail.com>
2019-01-15 09:14:13 +0100

    Connected the Frontend to the graphql Backend

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Controll-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
@@ -22,7 +32,7 @@ app.use('/graphql', graphqlHttp({
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongocluster-wufx9.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
   { useNewUrlParser: true })
     .then(() => {
-      app.listen(3000);
+      app.listen(8000);
       console.log('Connection to Atlas Successful!');
     })
     .catch(err => {
